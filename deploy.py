b33af5ed2e4c9a9a1d463e6f316e9e5fe08f2e6e
ablazleon <ablazleon@ono.com>
2019-01-01 01:39:41 +0100

    Draft commands

--- deploy.py
+++ deploy.py
@@ -18,7 +18,7 @@ def cli():
 @cli.command()
 def setupvnx():
     """Download the vnx xml of pfinal"""
-    click.echo("Download the vnx xml of pfinal")
+    logger.debug("Download the vnx xml of pfinal")
     line = "wget http://idefix.dit.upm.es/cdps/pfinal/pfinal.tgz"
     call(line, shell=True)
     line = "sudo vnx --unpack pfinal.tgz"
@@ -31,16 +31,35 @@ def setupvnx():
 @cli.command()
 def up():
     """Boot the system """
-    click.echo("Boot the system ")
+    logger.debug("Boot the system ")
     os.system("sudo vnx -f pfinal.xml --create")
     # os.system("sudo vnx -f pfinal.xml --show-map")
 
 @cli.command()
 def greet():
-    """Create a Hello.text in all machines and then show it."""
-    click.echo('=> hello in s1')
-    os.system("sudo lxc-attach --clear-env -n s1 -- bash -c \"echo 'hello' > hello.txt \"" )
-    os.system("sudo lxc-attach --clear-env -n s1 -- bash -c ls")
+    """Create & show Hello.text in s1,s2,s3,lb,bbdd,nas1,nas2 & nas3"""
+
+    for i in [1,2,3]:
+      logger.debug('=> hello in ' + str(i))
+      os.system("sudo lxc-attach --clear-env -n s"+ str(i) +" -- bash -c \"echo 'hello' > hello.txt \"" )
+      os.system("sudo lxc-attach --clear-env -n s"+ str(i) +" -- bash -c ls")
+
+    logger.debug('=> hello in lb')
+    os.system("sudo lxc-attach --clear-env -n lb -- bash -c \"echo 'hello' > hello.txt \"")
+    os.system("sudo lxc-attach --clear-env -n lb -- bash -c ls")
+
+    logger.debug('=> hello in fw')
+    os.system("sudo lxc-attach --clear-env -n fw -- bash -c \"echo 'hello' > hello.txt \"")
+    os.system("sudo lxc-attach --clear-env -n fw -- bash -c ls")
+
+    for i in [1,2,3]:
+      logger.debug('=> hello in nas' + str(i))
+      os.system("sudo lxc-attach --clear-env -n nas"+ str(i) +" -- bash -c \"echo 'hello' > hello.txt \"" )
+      os.system("sudo lxc-attach --clear-env -n nas"+ str(i) +" -- bash -c ls")
+
+    logger.debug('=> hello in bbdd')
+    os.system("sudo lxc-attach --clear-env -n bbdd -- bash -c \"echo 'hello' > hello.txt \"")
+    os.system("sudo lxc-attach --clear-env -n bbdd -- bash -c ls")
 
 @cli.command()
 def destroy():
@@ -48,3 +67,15 @@ def destroy():
     click.echo("Destroy the system ")
     os.system("sudo vnx -f pfinal.xml --destroy")
     # os.system("sudo vnx -f pfinal.xml --show-map")
+
+@cli.command()
+def confbbdd():
+    """Config the BBDD as in the statment"""
+
+@cli.command()
+def confcluster():
+    """Config gluster in nasX and in front servers"""
+
+@cli.command()
+def conffront():
+    """Config the front servers"""
